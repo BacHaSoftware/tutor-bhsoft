f7715a577de00a3b1f00c626b41c61effe8eb3b4
Bach Hung Cong <congbach.bhtech@gmail.com>
2024-05-31 10:03:49 +0700

    Add BHSoft logo, favico

--- tutorbhsoft/plugin.py
+++ tutorbhsoft/plugin.py
@@ -52,5 +52,14 @@ hooks.Filters.ENV_PATCHES.add_items(
     RUN npm install '@edx/frontend-component-footer@git+https://github.com/BacHaSoftware/frontend-component-footer.git#main' --registry=$NPM_REGISTRY
     """
         ),
+        (
+            "mfe-lms-production-settings",
+            """
+    MFE_CONFIG["LOGO_URL"] = "https://bachasoftware.com/web/image/website/2/logo/Bac%20Ha%20Software%20Co.%2C%20Ltd.?unique=7bf7a33"
+    MFE_CONFIG["LOGO_TRADEMARK_URL"] = "https://bachasoftware.com/web/image/website/2/logo/Bac%20Ha%20Software%20Co.%2C%20Ltd.?unique=7bf7a33"
+    MFE_CONFIG["LOGO_WHITE_URL"] = "https://bachasoftware.com/web/image/website/2/logo/Bac%20Ha%20Software%20Co.%2C%20Ltd.?unique=7bf7a33"
+    MFE_CONFIG["FAVICON_URL"] = "https://bachasoftware.com/web/image/website/2/favicon?unique=33e1031"
+    """
+        )
     ]
 )
